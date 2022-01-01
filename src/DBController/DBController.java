3dd56b76817ce091160f8eb9f54b8d8eb5520591
TienCris-Hcmus <92070120+TienCris-Hcmus@users.noreply.github.com>
2022-01-01 19:41:36 +0700

    Update DBController.java

--- src/DBController/DBController.java
+++ src/DBController/DBController.java
@@ -6,9 +6,12 @@
 package DBController;
 
 
+import Class.DonHang;
+import Class.NhanVien;
 import Class.SanPham;
 import DBConnection.DBConnection;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -41,7 +44,7 @@ public class DBController {
         }
     }
     
-    public static ArrayList<DoiTac> getTenDoiTac(){
+    /*public static ArrayList<DoiTac> getTenDoiTac(){
         ArrayList<DoiTac> list = new ArrayList<>();
         Connection conn = null;
         try{
@@ -59,7 +62,7 @@ public class DBController {
             Logger.getLogger(DBController.class.getName()).log(Level.SEVERE, null, ex);
         }
         return list;
-    }
+    }*/
 
     public static ArrayList<SanPham> getSanPham1() {
         ArrayList<SanPham> list = new ArrayList<>();
@@ -105,4 +108,50 @@ public class DBController {
         return list;
     }
     
+    public static ArrayList<DonHang> getDonHang(String MANV) {
+        ArrayList<DonHang> list = new ArrayList<>();
+        Connection conn = null;
+        try{
+            conn = DBConnection.getConnection();
+            PreparedStatement ps=conn.prepareStatement("select NV.MaNV, TenNV, TongTien, NgayDat from NhanVien NV, DonHang DH where NV.MaNV=DH.MaNV and NV.MaNV=?");
+            ps.setString(1, MANV);
+            ResultSet rs=ps.executeQuery();
+            while(rs.next()){
+                String manv = rs.getString("MaNV");
+                String tennv = rs.getString("TenNV");
+                int tongtien = rs.getInt("TongTien");
+                Date ngaydat = rs.getDate("NgayDat");
+                DonHang dh;
+                dh = new DonHang(manv, tennv, tongtien, ngaydat);
+                list.add(dh);
+            }
+        }catch (SQLException ex) {
+            Logger.getLogger(DBController.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        return list;
+    }
+
+    public static ArrayList<NhanVien> getNhanVien(String MANV) {
+        ArrayList<NhanVien> list = new ArrayList<>();
+        Connection conn = null;
+        try{
+            conn = DBConnection.getConnection();
+            PreparedStatement ps=conn.prepareStatement("select NV.MaNV, TenNV, TrangThai from NhanVien NV, DiemDanhNV DDNV where NV.MaNV=DDNV.MaNV and NV.MaNV=?");
+            ps.setString(1, MANV);
+            ResultSet rs=ps.executeQuery();
+            while(rs.next()){
+                String manv = rs.getString("MaNV");
+                String tennv = rs.getString("TenNV");
+                String trangthai = rs.getString("TrangThai");
+                NhanVien nv;
+                nv = new NhanVien(manv, tennv, trangthai);
+                list.add(nv);
+            }
+        }catch (SQLException ex) {
+            Logger.getLogger(DBController.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        return list;
+    }
 }
